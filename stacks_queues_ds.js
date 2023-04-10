771e1132cac5286d7bd38c90bafb563b29d5c29d
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-10 19:26:05 +0100

    implementing stacks data structures using linked list and arrays

--- stacks_queues_ds.js
+++ stacks_queues_ds.js
@@ -1,4 +1,58 @@
 // Stack Data Structure using Linked List
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//     this.bottom = null;
+//     this.length = 0;
+//   }
+//   peek() {
+//     return this.top;
+//   }
+//   push(value) {
+//     const newNode = new Node(value);
+//     if (this.length === 0) {
+//       this.top = newNode;
+//       this.bottom = newNode;
+//     } else {
+//       const holdingPointer = this.top;
+//       this.top = newNode;
+//       this.top.next = holdingPointer;
+//     }
+//     this.length++;
+//     return this;
+//   }
+//   pop() {
+//     if (!this.top) {
+//       return null;
+//     }
+//     if (this.top === this.bottom) {
+//       this.bottom = null;
+//     }
+//     // const holdingPointer = this.top;
+//     this.top = this.top.next;
+//     this.length--;
+//     return this;
+//   }
+// }
+
+// const myStack = new Stack();
+// myStack.push("google");
+// myStack.push("udemy");
+// myStack.push("discord");
+// myStack.push("youtube");
+// myStack.peek();
+// myStack.pop();
+// myStack.pop();
+// console.log(myStack);
+
+// Stack Data Structure using Arrays
 class Node {
   constructor(value) {
     this.value = value;
@@ -8,36 +62,17 @@ class Node {
 
 class Stack {
   constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
+    this.array = [];
   }
   peek() {
-    return this.top;
+    return this.array[this.array.length - 1];
   }
   push(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      const holdingPointer = this.top;
-      this.top = newNode;
-      this.top.next = holdingPointer;
-    }
-    this.length++;
+    this.array.push(value);
     return this;
   }
   pop() {
-    if (!this.top) {
-      return null;
-    }
-    if (this.top === this.bottom) {
-      this.bottom = null;
-    }
-    // const holdingPointer = this.top;
-    this.top = this.top.next;
-    this.length--;
+    this.array.pop();
     return this;
   }
 }
@@ -47,7 +82,7 @@ myStack.push("google");
 myStack.push("udemy");
 myStack.push("discord");
 myStack.push("youtube");
+myStack.pop();
+myStack.pop();
 myStack.peek();
-myStack.pop();
-myStack.pop();
 console.log(myStack);
