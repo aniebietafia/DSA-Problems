ab0e6e082cb860f2020a56e062ed6bbf1e3c2533
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-11 14:57:40 +0100

    tree traversal data structure

--- trees_ds.js
+++ trees_ds.js
@@ -35,7 +35,22 @@ class BinarySearchTree {
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
   remove() {}
 }
 
@@ -47,7 +62,8 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-JSON.stringify(traverse(tree.root));
+tree.lookup(9);
+// JSON.stringify(traverse(tree.root));
 
 function traverse(node) {
   const tree = { value: node.value };
