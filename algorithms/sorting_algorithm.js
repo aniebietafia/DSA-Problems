582666059e3786acb4f35b679fa0dfdf168811c5
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-12 17:22:57 +0100

    Sorting algorithm

--- algorithms/sorting_algorithm.js
+++ algorithms/sorting_algorithm.js
@@ -18,6 +18,7 @@ console.log(nums);
 */
 
 // Selection sort
+/*
 const nums = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
@@ -31,6 +32,30 @@ function selectionSort(array) {
     array[i] = array[minimum];
     array[minimum] = temp;
   }
+  return array;
 }
 selectionSort(nums);
+console.log(nums);
+*/
+
+// Insertion sort
+const nums = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      // move number to the first position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // find where number should go
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          // move number to the right spot
+          array.splice(j, 0, array(i, 1)[0]);
+        }
+      }
+    }
+  }
+}
+insertionSort(nums);
 console.log(nums);
