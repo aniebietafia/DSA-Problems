898657c387175203ec8c4015ddcdf9e16554694d
aniebietafia <aniebietafia87@gmail.com>
2024-01-01 18:17:48 +0100

    min max sum problem

--- solutions/1-Easy/src/index.ts
+++ solutions/1-Easy/src/index.ts
@@ -1,5 +1,7 @@
 // import function
-import plusMinus from "./1-Plus-Minus";
+// import plusMinus from "./1-Plus-Minus";
+import miniMaxSum from "./2-Min-Max-Sum";
 
 // execute function
-plusMinus([-4, 3, -9, 0, 4, 1]);
+// plusMinus([-4, 3, -9, 0, 4, 1]);
+miniMaxSum([1, 3, 5, 7, 9]);
